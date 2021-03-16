45ee5fc4aa7d2bd28389ec8c003f27765717df6d
faustofcjr <fausto.fcjr@gmail.com>
2021-03-16 08:25:21 -0300

    hr-api-gateway project created

---
+++ hr-api-gateway/src/test/java/com/yesiv/gateway/HrApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesiv.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
