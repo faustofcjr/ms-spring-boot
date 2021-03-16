e6cec600e0f021d3975f59780a3c11e6bca46cc8
faustofcjr <fausto.fcjr@gmail.com>
2021-03-15 21:19:24 -0300

    Add Spring Cloud OpenFeign

--- hr-payroll/src/main/java/com/yesiv/hrworker/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/yesiv/hrworker/HrPayrollApplication.java
@@ -2,7 +2,9 @@ package com.yesiv.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
--- hr-payroll/src/main/java/com/yesiv/hrworker/services/PaymentService.java
+++ hr-payroll/src/main/java/com/yesiv/hrworker/services/PaymentService.java
@@ -1,29 +1,20 @@
 package com.yesiv.hrworker.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.yesiv.hrworker.entities.Payment;
 import com.yesiv.hrworker.entities.Worker;
+import com.yesiv.hrworker.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;	
 
 	public Payment getPayment(Long workId, int days) {
-		Map<String, String> parameters = new HashMap<>();
-		parameters.put("id", String.valueOf(workId));
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, parameters);
+		Worker worker = workerFeignClient.findById(workId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
