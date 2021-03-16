61d93d93b6f2424983022fd9065a80acdaf5b8de
faustofcjr <fausto.fcjr@gmail.com>
2021-03-15 23:09:23 -0300

    Eureka configuration

--- hr-eureka-server/src/main/java/com/yesiv/eureka/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/yesiv/eureka/HrEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.yesiv.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
