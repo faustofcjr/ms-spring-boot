2831c453cc09cb16ec01c47ded05d8200c3a0226
faustofcjr <fausto.fcjr@gmail.com>
2021-03-17 13:44:41 -0300

    project hr-user created

---
+++ hr-user/src/test/java/com/yesiv/user/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesiv.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
