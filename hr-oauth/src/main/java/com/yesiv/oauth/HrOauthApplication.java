fd6d567afc6e869c890a911173f7bf8f4be3b825
faustofcjr <fausto.fcjr@gmail.com>
2021-03-17 17:55:26 -0300

    configuring oauth project

--- hr-oauth/src/main/java/com/yesiv/oauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/yesiv/oauth/HrOauthApplication.java
@@ -2,8 +2,10 @@ package com.yesiv.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HrOauthApplication {
 
 	public static void main(String[] args) {
