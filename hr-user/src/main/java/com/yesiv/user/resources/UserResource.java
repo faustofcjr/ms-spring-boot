e8dd00541b3855f207ded12995db7bf908421cf9
faustofcjr <fausto.fcjr@gmail.com>
2021-03-17 17:45:34 -0300

    project hr-oauth created

--- hr-user/src/main/java/com/yesiv/user/resources/UserResource.java
+++ hr-user/src/main/java/com/yesiv/user/resources/UserResource.java
@@ -1,12 +1,6 @@
 package com.yesiv.user.resources;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.yesiv.user.entities.User;
 import com.yesiv.user.repositories.UserRepository;
-
 
 @RestController
 @RequestMapping(value = "/users")
@@ -30,11 +23,10 @@ public class UserResource {
 		User user = repository.findById(id).get();
 		return ResponseEntity.ok(user);
 	}
-	
+
 	@GetMapping(value = "/search")
 	public ResponseEntity<User> findByEmail(@RequestParam String email) {
 		User user = repository.findByEmail(email);
 		return ResponseEntity.ok(user);
 	}
 }
-
