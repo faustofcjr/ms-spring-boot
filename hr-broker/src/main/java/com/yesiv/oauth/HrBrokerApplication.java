c35b9ce3bff0e7fbf6f9a2d0b26245b5172945f7
faustofcjr <fausto.fcjr@gmail.com>
2021-04-23 06:43:54 -0300

    Creating message broker microservice.

---
+++ hr-broker/src/main/java/com/yesiv/oauth/HrBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.yesiv.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrBrokerApplication.class, args);
+	}
+
+}
